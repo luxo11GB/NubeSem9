68fee52236c5050019287d0c44f1918385d29072
luxo11GB <gzalesluis3@gmail.com>
2025-11-03 16:57:54 -0500

    Update national news details in nacional.php

--- api/secciones/nacional.php
+++ api/secciones/nacional.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $nacional = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+ "titulo" => "Gobierno anuncia nuevo plan de reactivación económica",
+  "autor" => "María Fernanda López",
+  "resumen" => "El Gobierno presentó un paquete de medidas para impulsar la economía en 2025, con énfasis en la generación de empleo, apoyo a pequeñas empresas y reducción de impuestos a sectores productivos."
 ];
 ?>
