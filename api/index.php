cd72595ef271163091f8de02a3fef4bd71ef20c4
luxo11GB <gzalesluis3@gmail.com>
2025-11-03 16:55:57 -0500

    Update print statement from 'Hello' to 'Goodbye'

--- api/index.php
+++ api/index.php
@@ -34,7 +34,7 @@
       ?>
       <div class="jumbotron p-3 p-md-5 text-white rounded bg-dark">
         <div class="col-md-12 px-0">
-          <h1 class="display-4 font-italic">
+          <h1 class="display-4 font-italic style="color: red;">
             <?php
               echo $portada["titulo"];
             ?>
@@ -579,3 +579,4 @@
     </script>
   </body>
 </html>
+
