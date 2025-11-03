def9d497792d7f6525659059bae2bc0146956493
luxo11GB <gzalesluis3@gmail.com>
2025-11-03 17:18:41 -0500

    Add background color style to portada section
    Added CSS style to change the background color of the portada section.

--- api/secciones/portada.php
+++ api/secciones/portada.php
@@ -4,9 +4,18 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $portada = [
-"titulo" => "Gonzales Berrocal Luis Angel",
-"autor" => "jperez",
-"resumen" => "Las noticias mas importantes del 2025",
+    "titulo" => "Gonzales Berrocal Luis Angel",
+    "autor" => "jperez",
+    "resumen" => "Las noticias mas importantes del 2025",
 ];
 ?>
+
+<style>
+  /* Cambia el fondo de la sección portada */
+  body {
+    background-color: #f0f8ff; /* color claro, cámbialo por el que quieras */
+  }
+</style>
+
+
 
