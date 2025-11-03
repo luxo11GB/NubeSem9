a91e2013b923964ccb8fc1a9d3107afd4599a74e
luxo11GB <gzalesluis3@gmail.com>
2025-11-03 16:58:53 -0500

    Update international news details in internacional.php

--- api/secciones/internacional.php
+++ api/secciones/internacional.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $internacional = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+ "titulo" => "Naciones Unidas lanza iniciativa global contra el cambio climático",
+  "autor" => "Thomas Müller",
+  "resumen" => "La ONU presentó un nuevo programa internacional para reducir las emisiones de carbono en un 40% antes de 2035. Más de 100 países firmaron el acuerdo, comprometiéndose a adoptar energías renovables y políticas sostenibles."
 ];
 ?>
